2767331b6835cc5eaf3b153df5e3c61b1126e0ff
Pranav Kale <pranavkale021998@gmail.com>
2024-07-14 15:11:37 +0530

    Time: 15 ms (68.2%), Space: 21.9 MB (11.82%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,26 +3,13 @@ public:
     void moveZeroes(vector<int>& nums) {
         if( nums.size() <= 1 ) return; 
         int l = 0; 
-        int r = 1;
-        int n = nums.size(); 
 
-        while( r < n ) {
-            if( nums[l] == 0 && nums[r]==0 ) { 
-                r++; 
-            }
-            else if( nums[l]==0 ) {
+        for( int r=0; r<nums.size(); r++ ) {
+            if( nums[r] != 0 ) {
                 // swapping element at l with element at r 
                 swap( nums[l], nums[r] );
-                if( r-1 == l ) r++;  
                 l++;                    
             }
-            else { 
-                l++; 
-                r++; 
-            }
-
-            
-            cout << l << " " << r << endl;
         } 
     }
 };
