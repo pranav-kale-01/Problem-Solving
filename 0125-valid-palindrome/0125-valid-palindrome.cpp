fb3f4b424b0400ecf4c517f74ae392c98acb44e3
Pranav Kale <pranavkale021998@gmail.com>
2024-07-19 19:16:22 +0530

    Time: 4 ms (79.5%), Space: 8.9 MB (39.68%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -4,23 +4,21 @@ public:
         int l=0; 
         int r=s.size()-1; 
 
-        while( l < r ) {
-
-            if( (s[l] < 65 || s[l] > 90) && ( s[l] < 97 || s[l] > 122 ) && (s[l] < 48 || s[l] > 57 ) ) { 
+        while( l <= r ) {
+            if( !isalnum(s[l] ) ) { 
                 l++; 
                 continue; 
             }
-            if( (s[r] < 65 || s[r] > 90) && ( s[r] < 97 || s[r] > 122 ) && (s[r] < 48 || s[r] > 57 ) ) { 
+            if( !isalnum(s[r]) ) { 
                 r--; 
                 continue; 
             }
 
             // coverting to lowwercase 
-            if( s[l] >= 65 && s[l] <= 90 )  s[l]+=32; 
-            if( s[r] >= 65 && s[r] <= 90 )  s[r]+=32;  
-            if( s[l] != s[r] ) return false; 
-            
-            l++; r--;
+            if( tolower( s[l] ) != tolower( s[r] ) ) return false; 
+            else {
+                l++; r--;
+            }
         }
         return true;
     }
