5aa489afe9c4c497a41cd02b42d2f0ea82061170
Pranav Kale <pranavkale021998@gmail.com>
2024-07-20 17:43:31 +0530

    Time: 15 ms (51.61%), Space: 20.7 MB (27.69%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -5,13 +5,10 @@ public:
         int total = (n * (n+1))/2;
         int nums_sum = 0; 
 
-        for( int i=0; i<n; i++ ) { 
+        for( int i=0; i<n; i++ )  
             nums_sum+=nums[i]; 
-        }
 
-        // now subtracting the nums_sums from sum and we will get the 
-        // missing number. If the sum is equal then the missing number is 0
-        if( nums_sum == total ) return 0; 
-        else return total - nums_sum;
+        // now subtracting the nums_sums from sum. 
+        return total - nums_sum;
     }
 };
