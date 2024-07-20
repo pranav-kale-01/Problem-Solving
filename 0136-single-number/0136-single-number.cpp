c38351a440483576f10d59930b27802b10f8dc27
Pranav Kale <pranavkale021998@gmail.com>
2024-07-20 18:03:22 +0530

    Time: 20 ms (23.41%), Space: 20.7 MB (17.34%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -5,9 +5,8 @@ public:
         sort( nums.begin(), nums.end() ); 
 
         for( int i=0; i+1<nums.size(); i+=2 ) {
-            if( nums[i] != nums[i+1] ) {
+            if( nums[i] != nums[i+1] ) 
                 return nums[i]; 
-            }
         }
 
         return nums.back();
