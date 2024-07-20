fc30396165cfd20ccdd5b5bf77b36c4c48426fe8
Pranav Kale <pranavkale021998@gmail.com>
2024-07-20 18:00:31 +0530

    Time: 24 ms (16.68%), Space: 20.8 MB (17.34%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        // sorting the vector 
+        sort( nums.begin(), nums.end() ); 
+
+        for( int i=0; i+1<nums.size(); i+=2 ) {
+            if( nums[i] != nums[i+1] ) {
+                return nums[i]; 
+            }
+        }
+
+        return nums.back();
+    }   
+};
