126ed27b33ee7142fae98eb4bec07aae2f311ce5
Pranav Kale <pranavkale021998@gmail.com>
2024-07-25 21:36:37 +0530

    Time: 10 ms (65.53%), Space: 17.2 MB (5.77%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // using a hashmap for storing the elements 
+        map<int, int> mp; 
+
+        for( int i=0; i<nums.size(); i++ ) 
+            mp[nums[i]] = i; 
+
+        // now subtracting the target from each element and 
+        // checking if the element is present in the map 
+        // and does not have the same index as the current elemnt 
+        // (since duplicates are not allowed)
+        vector<int> res;
+
+        for( int i=0; i<nums.size(); i++ ) { 
+            if( mp[ target - nums[i]] != NULL && i!=mp[ target - nums[i]] ) {
+                res.push_back( i ); 
+                res.push_back( mp[target - nums[i] ] );
+                break; 
+            }
+        }
+
+        return res; 
+    }
+};
